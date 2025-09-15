eb4743fdfcff83d37f11a6e300280b6542023be3
knjomo021 <knjomo021@student.wethinkcode.co.za>
2025-09-15 20:12:43 +0200

    Add Arguments script

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
-const args = ProcessingInstruction.argv.length -2;
+const argsCount = process.argv.length - 2;
 
-if (args === 0) {
+if (argsCount === 0) {
   console.log('No argument');
-} else if (args === 1) {
+} else if (argsCount === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
