e1e1ac6e6edcd14a00f7a021e83248ddc3ed707b
knjomo021 <knjomo021@student.wethinkcode.co.za>
2025-09-17 13:40:23 +0200

    Added Multi-C

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,6 @@
-#!/usr/bin/node
-
 const times = parseInt(process.argv[2], 10);
 
-if (isNaN(times) || times <= 0) {
-  console.log('Missing number of occurrences');
-} else {
+if (times > 0) {
   let i = 0;
   while (i < times) {
     console.log('C is fun');
